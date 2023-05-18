8858e475fd3b6b684bcc47f879a4778c4e7db4ae
NastyonaSavina <nastyona.savina@gmail.com>
2023-05-18 10:21:48 +0300

    Update toString.js

--- src/toString.js
+++ src/toString.js
@@ -5,5 +5,10 @@ const uniqueItem = { toString: Math.random };
 
 
 
-// console.log(test(uniqueItem));
+console.log(test(uniqueItem));
 
+
+
+//логіка в тому що, на змінній x викликається метод toString, 
+// через те що вона всередині шаблонної строки.
+// Його просто треба переписати на кастомний, ось і все
