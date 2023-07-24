8f60afe19f9c5d770639d8dfa3d9da822b11c964
NastyonaSavina <103425739+NastyonaSavina@users.noreply.github.com>
2023-07-24 16:18:12 +0000

    Deploying to gh-pages from @ NastyonaSavina/TrickyTasks.js@66e51f10a7b85c5870843053bb3c722099845b3e 🚀

---
+++ index.98005f57.js
@@ -0,0 +1,2 @@
+!function(){var i;(i="radency").match(/[aeiouy]/gi).toString().split(",").join(""),i.match(/[^aeiouy]/gi).toString().split(",").join("")}();
+//# sourceMappingURL=index.98005f57.js.map
---
+++ index.b4d005bb.js
@@ -0,0 +1,2 @@
+!function(i,t,n){const o=i.match(/[aeiouy]/gi).toString().split(",").join(""),a=i.match(/[^aeiouy]/gi).toString().split(",").join("")}("radency","rdnc","aey");
+//# sourceMappingURL=index.b4d005bb.js.map
